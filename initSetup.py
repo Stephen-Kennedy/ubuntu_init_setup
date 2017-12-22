ba7ea6d32e84b292172a2f2616cd6156081d8742
Stephen <medicup@gmail.com>
2017-12-22 10:11:01 -0500

    Update and rename initSetup.sh to initSetup.py

---
+++ initSetup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Author Stephen J Kennedy
+# Version 1.0
+# Script for installing default packages with Python
+import os
+
+def install_default():
+  os.system('apt -y update')
+  cache = apt.cache() # get the current installed packages from apt
+  programs = ['vim', 'dnsutils', 'ccze', 'iftop', 'htop', 'curl', 'openssh-client']
+
+  for program in programs:
+    if cache["%s" % program].is_installed:
+      print("Skipping %s, it is already installed. " % (program))
+    else:
+      print("Installing %s " % program)
+      os.system('apt-get install -y %s' % (program))
+      
+install_default()
