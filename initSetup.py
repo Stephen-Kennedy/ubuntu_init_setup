e4e75db2e2216496c4f8d61f730ccb53cf97a87f
Stephen <medicup@gmail.com>
2022-04-10 22:48:34 -0400

    Update initSetup.py

--- initSetup.py
+++ initSetup.py
@@ -17,5 +17,5 @@ def install_default():
             print(f'Installing {program}')
             os.system(f'apt-get install -y {program}')
 
-install_default()!/usr/bin/python3
+install_default()
 
