ff3c05974f7175ec97e64191e02d84f43d7d6906
Stephen <medicup@gmail.com>
2022-04-10 22:39:47 -0400

    Update initSetup.py

--- initSetup.py
+++ initSetup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 # Author Stephen J Kennedy
-# Version 2.0
+# Version 1.0
 # Script for installing default packages with Python
 import os
 import apt
 
 def install_default():
     cache = apt.Cache() # get the current installed packages from apt
-    programs = ['vim', 'dnsutils', 'ccze', 'iftop', 'htop', 'curl', 'openssh-server', 'openssh-client', 'iptables-persi>
+    programs = ['vim', 'dnsutils', 'ccze', 'iftop', 'htop', 'curl', 'openssh-server', 'openssh-client', 'iptables-persistent', 'python3-pip']
+
     for program in programs:
         if cache[program].is_installed:
             print(f"Skipping {program}, it is already installed. ")
