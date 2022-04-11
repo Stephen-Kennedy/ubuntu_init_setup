d0eb948f05b9c45d97d9a28b371306dbe2ca23bd
Stephen <medicup@gmail.com>
2022-04-10 22:43:01 -0400

    Update initSetup.py

--- initSetup.py
+++ initSetup.py
@@ -7,7 +7,8 @@ import apt
 
 def install_default():
     cache = apt.Cache() # get the current installed packages from apt
-    programs = ['vim', 'dnsutils', 'ccze', 'iftop', 'htop', 'curl', 'openssh-server', 'openssh-client', 'iptables-persistent', 'python3-pip']
+    programs = ['vim', 'dnsutils', 'ccze', 'iftop', 'htop', 'curl', 'openssh-server', 'openssh-client', 
+                'iptables-persistent', 'python3-pip']
 
     for program in programs:
         if cache[program].is_installed:
